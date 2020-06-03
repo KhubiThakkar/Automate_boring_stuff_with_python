a0dbc3fe96fe1f45b3c9b0f84ff7d26b78593b15
Khubi Thakkar <khubithakkar@gmail.com>
2020-06-03 21:54:10 +0530

    Chap2 Flow Control

---
+++ Chap2/exitExample.py
@@ -0,0 +1,7 @@
+import random
+while True:
+    print('Type exit to exit')
+    enter = input()
+    if enter == 'exit':
+        sys.exit()
+    print('You typed '+enter+'.')
---
+++ Chap2/yourName.py
@@ -0,0 +1,6 @@
+while True:
+    print('Plese enter your name')
+    name = input()
+    if name == 'your name':
+        break
+    print('Thank You!') 
