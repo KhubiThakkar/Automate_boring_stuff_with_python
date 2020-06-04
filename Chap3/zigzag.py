c7f20472e790b023c99b3a050e002b7e3774adf9
Khubi Thakkar <khubithakkar@gmail.com>
2020-06-04 23:03:06 +0530

    Chap3 Functions

--- Chap3/zigzag.py
+++ Chap3/zigzag.py
@@ -5,17 +5,17 @@ try:
     while True:
         i=0
         while i <= indent:
-            print('a', end='')
+            print(' ', end='')
             i = i+1
         print('********')
         time.sleep(0.1)
-    if indentIncreasing:
-        indent = indent + 1
-        if indent == 20:
-            indentIncreasing = False
-    else:
-        indent = indent-1
-        if indent == 0:
-            indentIncreasing = True
+        if indentIncreasing:
+            indent = indent + 1
+            if indent == 20:
+                indentIncreasing = False
+        else:
+            indent = indent-1
+            if indent == 0:
+                indentIncreasing = True
 except KeyboardInterrupt:
     sys.exit()
